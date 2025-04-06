1b508a11a4b2ca34db8a24562722bd49577c8832
bbbbhceg <132533636+bbbbhceg@users.noreply.github.com>
2025-04-06 20:03:55 +0800

    Add files via upload
    复习和新学时候，更新了以前的相关东西，以补充更新知识体系。

--- python数据分析/numpy/06-2正态分布.py
+++ python数据分析/numpy/06-2正态分布.py
@@ -2,16 +2,17 @@ import numpy
 from pyecharts.charts import  Bar
 from pyecharts.options import *
 
-data1 = numpy.random.normal(loc=1.75,scale=0.2,size=1000)
+data1 = numpy.random.normal(loc=175,scale=15,size=1000)
 data1 = list(data1)
 data1 = sorted(data1)
 data_x = []
-for x in range(0,1000):
+for x in range(0,30):
     data_x.append(x)
 
 bar = Bar()
 bar.add_xaxis(data1)
 bar.add_yaxis("正态分布",data1,label_opts=LabelOpts(is_show=False))
+# 峰谷相消水平线在175附近
 
 
 bar.render("正态分布直方图.html")
--- python数据分析/numpy/07-1数组的切片索引.py
+++ python数据分析/numpy/07-1数组的切片索引.py
@@ -1,17 +1,44 @@
 import numpy
 
 list = [
-  [80,89,86,67,79],[78,97,89,67,81],
-  [90,94,78,67,74],[91,91,90,67,69],
-  [76,87,75,67,86],[70,79,84,67,84],
-  [94,92,93,67,64],[86,85,83,67,80]
+  [80,89,86,67,79],
+  [78,97,89,67,81],
+  [90,94,78,67,74],
+  [91,91,90,67,69],
+  [76,87,75,67,86],
+  [70,79,84,67,84],
+  [94,92,93,67,64],
+  [86,85,83,67,80]
 ]
 list = numpy.array(list)
 
 list_2 = list[2,:3]    # 对第三个列表的前三个元素进行切片，赋予list_2
-list_3 = list[1][:3]   # 对第二个列表的前三个元素进行切片。赋予list_3
-# list[2,:3] = 99999999  # 多元素批量修改      #####我在此发现切片索引，好像真的是索引并没有真的copy数值，在切片之后变动切片出，最后打印切片也会随着变化
 print(list_2)   # [90 94 78]
+
+list_3 = list[1][:3]   # 对第二个列表的前三个元素进行切片。赋予list_3
 print(list_3)   # [78 97 89]
 
+list_4  = list[2,[0,2,4,2,4,0] ] # 访问第3个列表的，下标为0,2,4的三个元素进行访问。
+print(list_4)   # [78 97 89]
+# list[2,:3] = 99999999  # 多元素批量修改
+# 我在此发现切片索引，索引并没有真的copy数值。
+# 证据是，在切片之后，不进行立刻打印，而是变动切片处，最后打印切片也会随着变化
+
+
+
+#  当数组是数值数组时，可以进行比较运算
+xxx = list > 90
+print(xxx)
+"""
+[[False False False False False]
+ [False  True False False False]
+ [False  True False False False]
+ [ True  True False False False]
+ [False False False False False]
+ [False False False False False]
+ [ True  True  True False False]
+ [False False False False False]]
+"""
+aaa = list[list > 90]
+print(aaa)  # [97 94 91 91 94 92 93]
 
